dcb17fb089facd3f774b08635b35d3167bc34c90
Valéry Farcy <vfarcy@free.fr>
2014-11-11 09:50:39 +0100

    updates

--- Pinclone.py
+++ Pinclone.py
@@ -4,21 +4,18 @@ from sqlalchemy import Column, Integer, Text
 
 app = Flask(__name__)
 
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///pin.db'
-#app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///new.db'
-#app.config['SQLACHEMY_DATABASE_URI'] = 'jdbc:sqlite:///pin.db'
-
+app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite://pin.db'
 db = SQLAlchemy(app)
-
 
 class Pin(db.Model):
     id = Column(Integer, primary_key=True)
     title = Column(Text, unique=False)
     image = Column(Text, unique=False)
 
-#app.debug = True
-
 db.create_all()
+
+
+#app.debug = True
 
 @app.route('/')
 def hello_world():
